4ddb831b034d57c0349839965ca3644d9557e08f
mahmoudfathy557 <mahmoudfathy557@gmail.com>
2020-08-03 23:41:08 +0200

    footer added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Error from './pages/Error'
 import Navbar from './components/Navbar'
 
 import { Switch, Route } from 'react-router-dom'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route exact path='/rooms/:slug' component={SingleRoom} />
         <Route component={Error} />
       </Switch>
+      <Footer />
     </>
   )
 }
